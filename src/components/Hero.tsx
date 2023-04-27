83dd0ce7bcf7433e41f3ca1f73d6044baf361bfa
Liborzdev <liborio07zs@gmail.com>
2023-04-26 19:37:46 -0700

    added transition effect with motion
    fixed mobile's responsiveness mode

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,61 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import ineamLogo from "../assets/images/ineamLogo.png";
 import StyleBlock from "@/components/RiFill";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const [text, setText] = useState("Aeronautica");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setText(prevText => prevText === "Aeronautica" ? "Aeroespacial" : "Aeronautica");
+    }, 3000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
-    <section id="home" className="min-h-[90vh] grid grid-cols-1 xl:grid-cols-8">
-      {/* Information */}
-      <div className="md:col-span-5 flex items-center justify-center p-8 xl:p-16">
-        <div className="flex flex-col gap-8">
-          <h1 className="text-5xl xl:text-6xl font-bold xl:leading-[7.5rem]">
-            Ingenieros en electrónica
-            <StyleBlock text={'Aeronautica'}/>
-            <span className='flex'>
-            de México, AC
-            </span>
-          </h1>
-          <p className="text-gray-500 text-2xl leading-[2.5rem]">
-            "La ingeniería electrónica al servicio de la seguridad aérea"
-          </p>
-          <div className="flex flex-col md:flex-row items-center gap-4">
-            <button
-              className="w-full xl:w-auto bg-blue-800 text-white py-4 px-8 rounded-xl text-xl hover:bg-sky-600 transition-all duration-300 ease-in">
-              <a href="#contact">Contáctanos</a>
-            </button>
+    <main className="flex justify-center items-center max-w-7xl mx-auto">
+      <section
+        id="home"
+        className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-10 md:gap-72 mx-auto py-16 md:py-32"
+      >
+        {/* Information */}
+        <div className="px-10 py-10 mx-auto">
+          <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="flex flex-col gap-8"
+          >
+            <h1 className="text-5xl xl:text-6xl font-bold leading-tight md:leading-[7.5rem]">
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.2, duration: 0.8 }}
+              >
+                Ingenieros en electrónica
+              </motion.span>
+
+
+              <StyleBlock text={text} />
+
+              <span className="flex">de México, AC</span>
+            </h1>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.4, duration: 0.5 }}
+              className="text-gray-500 text-2xl leading-tight md:leading-[2.5rem]"
+            >
+              "La ingeniería electrónica al servicio de la seguridad aérea"
+            </motion.p>
+            <div className="flex flex-col md:flex-row items-center gap-4">
+              <button
+                className="w-full xl:w-auto bg-blue-800 text-white py-4 px-8 rounded-xl text-xl hover:bg-sky-600 transition-all duration-300 ease-in"
+              >
+                <a href="#contact">Contáctanos</a>
+              </button>
+            </div>
+          </motion.div>
+        </div>
+        {/* Image */}
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+
+          transition={{ duration: 0.8 }}
+          className="flex flex-col justify-center items-center gap-8"
+
+        >
+
+          <div className="flex flex-col ">
+            {/* Content image */}
+            <Image
+              src={ineamLogo}
+              alt={"Logo ineam"}
+              className={
+                "w-[250px] h-[250px] md:w-[450px] md:h-[450px] object-cover hover:scale-110 transition-all duration-500 ease-in-out"
+              }
+              quality={100}
+              priority={true}
+            />
           </div>
-        </div>
-      </div>
-      {/* Image */}
-      <div className="md:col-span-3 flex items-center justify-center relative">
-        {/* Content image */}
-        <div>
-          <Image src={ineamLogo} alt={'Logo ineam'}
-                 className={"w-[250px] h-[250px] md:w-[550px] md:h-[550px] object-cover xl:-mt-28 hover:scale-110 transition-all duration-500 ease-in-out"}
-                 quality={100}
-                 priority={true}
-
-          />
-
-
-        </div>
-        {/* Maybe a circle */}
-        {/*<div*/}
-        {/*  className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[280px] h-[280px] md:w-[380px] md:h-[380px] bg-sky-600 border-[10px] border-primary rounded-full -z-10"></div>*/}
-
-        {/* Icons */}
-        {/*<div className="hidden md:block">*/}
-
-        {/*  <RiCheckboxBlankCircleFill*/}
-        {/*    className={'w-10 h-10 md:w-40 text md:h-32 text-sky-600 rounded-full border-gray-600 absolute top-[12%] right-[20%] xl:right-[10%]'}/>*/}
-        {/*  <RiCheckboxBlankCircleFill*/}
-        {/*    className={'w-10 h-10 md:w-20 md:h-20 text-sky-600 rounded-full border-gray-600 absolute top-[10%] xl:top-[2%] left-[20%] xl:left-[10%]'}/>*/}
-        {/*  <RiCheckboxBlankCircleFill*/}
-        {/*    className={'w-10 h-10 md:w-52 md:h-36 text-sky-600 rounded-full absolute bottom-[5%] left-[15%] xl:left-[3%] -rotate-12'}/>*/}
-        {/*</div>*/}
-      </div>
-    </section>
+        </motion.div>
+      </section>
+    </main>
   );
 };
 
--- src/components/RiFill.tsx
+++ src/components/RiFill.tsx
@@ -1,27 +1,43 @@
-import {useState} from 'react';
-import {RiCheckboxBlankCircleFill} from 'react-icons/ri';
-import styles from '@/components/Aeronautica.module.css';
+import { RiCheckboxBlankCircleFill } from 'react-icons/ri';
+import { motion } from 'framer-motion';
 
-// @ts-ignore
-const StyleBlock = ({text}) => {
+const StyleBlock = ({ text }: any) => {
+  const variants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+  };
+
   return (
-    <span
-      className="text-sky-600 py-2 px-6 border-8 border-sky-600 relative inline-block group"
+    <motion.span
+      className="inline-block relative group"
+      whileTap={{ scale: 0.9 }}
+      transition={{ duration: 0.2 }}
     >
-      {text}
-      <RiCheckboxBlankCircleFill
-        className="text-white text-base absolute -left-5 -top-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
-      />
-      <RiCheckboxBlankCircleFill
-        className="text-white text-base absolute -right-5 -top-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
-      />
-      <RiCheckboxBlankCircleFill
-        className="text-white text-base absolute -right-5 -bottom-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
-      />
-      <RiCheckboxBlankCircleFill
-        className="text-white text-base absolute -left-5 -bottom-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
-      />
-    </span>
+      <motion.span
+        className="text-sky-600 py-2 px-2 -rotate-1 border-8 max-w-[19rem] md:max-w-full border-sky-600 relative inline-block group"
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        key={text}
+      >
+        {text}
+        <span>
+
+          <RiCheckboxBlankCircleFill
+            className="text-white text-base absolute -left-5 -top-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
+          />
+          <RiCheckboxBlankCircleFill
+            className="text-white text-base absolute -right-5 -top-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
+          />
+          <RiCheckboxBlankCircleFill
+            className="text-white text-base absolute -right-5 -bottom-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
+          />
+          <RiCheckboxBlankCircleFill
+            className="text-white text-base absolute -left-5 -bottom-5 p-2 bg-sky-600 rounded-full box-content group-hover:block"
+          />
+        </span>
+      </motion.span>
+    </motion.span>
   );
 };
 
