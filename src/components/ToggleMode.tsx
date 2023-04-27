8547717cc2f0d3136313b0f6a1c0c99a965531ab
Liborzdev <liborio07zs@gmail.com>
2023-04-27 12:53:58 -0700

    new component toggle light/dark mode
    configured toggle, theme provider wrap main component

---
+++ src/components/ToggleMode.tsx
@@ -0,0 +1,24 @@
+import { useTheme } from 'next-themes';
+import { useState, useEffect } from 'react';
+
+const ThemeToggler = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+  return (
+    <button
+      className="w-10 h-10 bg-blue-100 rounded-lg dark:bg-slate-800 flex items-center justify-center hover:ring-2 ring-blue-400 transition-all duration-300 focus:outline-none"
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      aria-label="Toggle Dark Mode"
+    >
+      {theme === 'light' ? (
+        '🌙'
+      ) : (
+        '☀️'
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggler;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
-import {Head, Html, Main, NextScript} from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <title>Ineam</title>
-      <meta name="description" content="INEAM"/>
-      <meta name="keywords" content="INEAM"/>
-      <meta name="author" content="INEAM"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-      <Head/>
+      <meta name="description" content="INEAM" />
+      <meta name="keywords" content="INEAM" />
+      <meta name="author" content="INEAM" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <Head />
       <body>
-      <Main/>
-      <NextScript/>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   )
