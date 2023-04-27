dd70a1d5c952452f4fad0723c09021b9ebd89c25
Liborzdev <liborio07zs@gmail.com>
2023-04-27 11:55:45 -0700

    added card

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import ineamLogo from "../assets/images/ineamLogo.png";
 import StyleBlock from "@/components/RiFill";
 import { motion } from "framer-motion";
+import { RiCheckboxBlankCircleFill, RiStarFill } from 'react-icons/ri';
 
 const Hero = () => {
   const [text, setText] = useState("Aeronautica");
@@ -51,7 +52,7 @@ const Hero = () => {
             </motion.p>
             <div className="flex flex-col md:flex-row items-center gap-4">
               <button
-                className="w-full xl:w-auto bg-blue-800 text-white py-4 px-8 rounded-xl text-xl hover:bg-sky-600 transition-all duration-300 ease-in"
+                className="w-full xl:w-auto bg-blue-700 text-white py-4 px-8 rounded-xl text-xl hover:bg-blue-800 transition-all duration-300 ease-in"
               >
                 <a href="#contact">Contáctanos</a>
               </button>
@@ -62,25 +63,47 @@ const Hero = () => {
         <motion.div
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
-
           transition={{ duration: 0.8 }}
-          className="flex flex-col justify-center items-center gap-8"
-
+          className="flex flex-col justify-center items-center gap-8 relative"
         >
-
-          <div className="flex flex-col ">
+          <div className="flex flex-col">
             {/* Content image */}
             <Image
               src={ineamLogo}
               alt={"Logo ineam"}
               className={
-                "w-[250px] h-[250px] md:w-[450px] md:h-[450px] object-cover hover:scale-110 transition-all duration-500 ease-in-out"
+                "w-[250px] h-[250px] md:w-[550px] md:h-[450px] object-cover hover:scale-105 transition-all duration-500 ease-in-out"
               }
               quality={100}
               priority={true}
             />
+            {/* Content image */}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.6, duration: 0.7 }}
+            >
+              <div>
+                <div className="relative text-center bg-white shadow-xl rounded-lg p-4 flex flex-col justify-center gap-2 max-w-[250px] mx-auto -mt-4 lg:-mt-12 lg:ml-20 transition-all duration-500 ">
+                  <h2 className="text-xl font-bold tracking-[1px] text-gray-800">
+                    +50 años de experiencia
+                  </h2>
+                  <div className="flex items-center text-center justify-center gap-2 text-lg text-gray-500">
+                    <RiStarFill className="text-sky-600" />
+                    <RiStarFill className="text-sky-600" />
+                    <RiStarFill className="text-sky-600" />
+                    <RiStarFill className="text-sky-600" />
+                    <RiStarFill className="text-sky-600" />
+                  </div>
+
+                </div>
+              </div>
+            </motion.div>
           </div>
+          {/* Circle */}
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:-translate-x-8 w-[70vw] h-[70vw] md:w-[380px] md:h-[380px] lg:-translate-x-60 bg-white border-[10px] border-sky-600/50 rounded-full -z-10"></div>
         </motion.div>
+
       </section>
     </main>
   );
