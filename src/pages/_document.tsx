f100e220e1f9af1845af22e34baacb03cc40a4f2
zusldev-laptop <liboriozsoto@gmail.com>
2023-04-27 23:41:55 -0700

    added meta and delete title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,17 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Ineam</title>
-      <meta name="description" content="INEAM" />
-      <meta name="keywords" content="INEAM" />
-      <meta name="author" content="INEAM" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <Head />
+
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="INEAM - Ingenieros en Electronica Aeronautica de Mexico, A.C."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
